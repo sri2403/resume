42d8eca58ddd72b21517fdca191efa8bb2bed4b6
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-19 21:35:21 +0530

    first

--- Components/UserDetails.jsx
+++ Components/UserDetails.jsx
@@ -4,7 +4,7 @@ const UserDetails = () => {
     return (
         <div className='text-center'>
             <h3>SRI HARINI MUTHUKRISHNAN</h3>
-            <p>2/279, S.S.Nagar, Gfix opp, Melur, Madurai-625106_ 6381787438_ srihairnimuthukrishnan@gmail.com</p>
+            <p>Contact: 6381787438, srihairnimuthukrishnan@gmail.com</p>
             <p><a href="https://github.com/sri2403" target='_'>GitHub</a> | <a href='https://www.linkedin.com/in/sriharini-muthukrishnan-122259290/' target='_'>LinkedIn</a> | <a href='/' target='_'>My Portfolio</a></p><hr></hr>
         </div>
     );
