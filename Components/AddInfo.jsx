69fde781db25d1e41767f14fa75e9a353aba43bf
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-27 10:50:24 +0530

    done

--- Components/AddInfo.jsx
+++ Components/AddInfo.jsx
@@ -10,7 +10,6 @@ ExpressJs, MongoDB, MySQL</li>
                 <li><strong>Soft Skills:</strong> Communication, Problem-Solving, Collaboration and teamwork, Continuous
 learning mindset</li>
                 <li><strong>Certifications:</strong> Zen Full-stack Developer</li>
-                <li><strong>Languages:</strong> Tamil, English, Hindi(R)</li>
                 <li><strong>Tool Used:</strong> GIT, VSCode, Netlify, Render, Postman</li>
             </ul>
         </div>
--- Components/WorkExp.jsx
+++ Components/WorkExp.jsx
@@ -9,8 +9,8 @@ const WorkExp = () => {
                 <li>I have been learning MERN stack for the past 5 months</li>
                 <li>I have successfully completed some mini projects including <strong>Login and Registration Authentication System with Password reset,  Redux-based Shopping Cart,  To-do Application, Calculator and Weather API</strong>, with all featuring responsive web design for both
 phones and desktops, along with web performance optimization for Single-page
-applications. Additionally, I have utilized debugging techniques and Browser Developer Tools
-to ensure smooth functionality and performance.</li>
+applications.</li>
+                <li>My major project is an <strong>HR Management Dashboard</strong> using ReactJS, NodeJS, and MongoDB. The dashboard includes various features such as employee management, attendance tracking, leave management, and performance reviews.</li>
             </ul><hr></hr>
         </div>
     );
