c7183ca56e27b2d9b3f925d3058949124a13234c
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-30 12:25:34 +0530

    last

--- Components/UserDetails.jsx
+++ Components/UserDetails.jsx
@@ -4,7 +4,7 @@ const UserDetails = () => {
     return (
         <div className='text-center'>
             <h3>SRI HARINI MUTHUKRISHNAN</h3>
-            <p>Contact: 6381387438, srihairnimuthukrishnan@gmail.com</p>
+            <p>Contact: 6381387438, sriharinimuthukrishnan@gmail.com</p>
             <p><a href="https://github.com/sri2403" target='_'>GitHub</a> | <a href='https://www.linkedin.com/in/sriharini-muthukrishnan-122259290/' target='_'>LinkedIn</a> | <a href='https://portfolio-sri2403.netlify.app/' target='_'>My Portfolio</a></p><hr></hr>
         </div>
     );
