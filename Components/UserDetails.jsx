47ce85f0094d4535c3a8df96ddf6e55dd466c1b3
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-07-26 19:43:11 +0530

    imd added

--- Components/UserDetails.jsx
+++ Components/UserDetails.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import img from "../images/photo.jpg";
+import './UserDetails.css'; // Make sure to import the CSS file
 
 const UserDetails = () => {
     return (
-        <div className='text-center'>
-            <h3>SRI HARINI MUTHUKRISHNAN</h3>
-            <p>Contact: 6381387438, sriharinimuthukrishnan@gmail.com</p>
-            <p><a href="https://github.com/sri2403" target='_'>GitHub</a> | <a href='https://www.linkedin.com/in/sriharini-muthukrishnan-122259290/' target='_'>LinkedIn</a> | <a href='https://portfolio-sri2403.netlify.app/' target='_'>My Portfolio</a></p><hr></hr>
+        <div className='user-details'>
+            <div className='details'>
+                <header>
+                    <h3>SRI HARINI MUTHUKRISHNAN</h3>
+                    <p>Contact: 6381387438, sriharinimuthukrishnan@gmail.com</p>
+                </header>
+                <section>
+                    <p>
+                        <a href="https://github.com/sri2403" target='_blank' rel='noopener noreferrer'>GitHub</a> | 
+                        <a href='https://www.linkedin.com/in/sriharini-muthukrishnan-122259290/' target='_blank' rel='noopener noreferrer'>LinkedIn</a> | 
+                        <a href='https://portfolio-sri2403.netlify.app/' target='_blank' rel='noopener noreferrer'>My Portfolio</a>
+                    </p>
+                </section>
+            </div>
+            <img 
+                src={img} 
+                alt='Sri Harini Muthukrishnan' 
+                className='profile-img' 
+            />
         </div>
     );
 };
 
 export default UserDetails;
-
-
-
